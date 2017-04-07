8f9776e5c0dbdcdfd255a24eb2c3370c866523b3
Jean André Gauthier <jean.andre.gauthier@gmail.com>
2017-04-07 17:10:58 +0800

    Added basic support for FP geometry

--- Templates/src/main/java/ja/gauthier/competitiveprogramming/TemplateComputationalGeometry.java
+++ Templates/src/main/java/ja/gauthier/competitiveprogramming/TemplateComputationalGeometry.java
@@ -7,6 +7,99 @@ public class TemplateComputationalGeometry {
 
   abstract static class Point {}
 
+  static class PointDouble extends Point implements Comparable<PointDouble> {
+    double x;
+    double y;
+
+    PointDouble() {
+      this.x = 0;
+      this.y = 0;
+    }
+
+    PointDouble(double x, double y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    PointDouble(long x, long y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    PointDouble(PointDouble other) {
+      this.x = other.x;
+      this.y = other.y;
+    }
+
+    PointDouble(PointLong other) {
+      this.x = other.x;
+      this.y = other.y;
+    }
+
+    PointLong asPointLong() {
+      return new PointLong(this);
+    }
+
+    public int compareTo(PointDouble other) {
+      if (other == null) {
+        throw new NullPointerException();
+      } else if (this.equals(other)) {
+        return 0;
+      } else if (smallerThan(this, other)) {
+        return -1;
+      } else {
+        return 1;
+      }
+    }
+
+    void divideBy(double factor) {
+      this.x /= factor;
+      this.y /= factor;
+    }
+
+    public boolean equals(Object other) {
+      if (this == other) {
+        return true;
+      } else if (other == null || getClass() != other.getClass()) {
+        return false;
+      } else {
+        PointDouble otherPointDouble = (PointDouble) other;
+        return this.x == otherPointDouble.x && this.y == otherPointDouble.y;
+      }
+    }
+
+    public int hashCode() {
+      long xLongBits = Double.doubleToLongBits(this.x);
+      long yLongBits = Double.doubleToLongBits(this.y);
+      return (int) (31 * (xLongBits ^ (xLongBits >>> 32)) + (int) (yLongBits ^ (yLongBits >>> 32)));
+    }
+
+    void minus() {
+      this.x = -this.x;
+      this.y = -this.y;
+    }
+
+    void minus(PointDouble other) {
+      this.x -= other.x;
+      this.y -= other.y;
+    }
+
+    void plus(PointDouble other) {
+      this.x += other.x;
+      this.y += other.y;
+    }
+
+    void times(double factor) {
+      this.x *= factor;
+      this.y *= factor;
+    }
+
+    void times(PointDouble other) {
+      this.x *= other.x;
+      this.y *= other.y;
+    }
+  }
+
   static class PointLong extends Point implements Comparable<PointLong> {
     long x;
     long y;
@@ -16,14 +109,28 @@ public class TemplateComputationalGeometry {
       this.y = 0;
     }
 
+    PointLong(double x, double y) {
+      this.x = Math.round(x);
+      this.y = Math.round(y);
+    }
+
     PointLong(long x, long y) {
       this.x = x;
       this.y = y;
     }
 
+    PointLong(PointDouble other) {
+      this.x = Math.round(other.x);
+      this.y = Math.round(other.y);
+    }
+
     PointLong(PointLong other) {
       this.x = other.x;
       this.y = other.y;
+    }
+
+    PointDouble asPointDouble() {
+      return new PointDouble(this);
     }
 
     public int compareTo(PointLong other) {
@@ -38,8 +145,9 @@ public class TemplateComputationalGeometry {
       }
     }
 
-    PointLong divideBy(long factor) {
-      return new PointLong(this.x / factor, this.y / factor);
+    void divideBy(long factor) {
+      this.x = (long) Math.round(this.x / (double) factor);
+      this.y = (long) Math.round(this.y / (double) factor);
     }
 
     public boolean equals(Object other) {
@@ -83,94 +191,188 @@ public class TemplateComputationalGeometry {
     }
   }
 
+  static double angle(PointDouble a) {
+    return Math.atan2(a.y, a.x);
+  }
+
   static double angle(PointLong a) {
     return Math.atan2(a.y, a.x);
+  }
+
+  static double angle(PointDouble a, PointDouble b) {
+    return Math.atan2(crossProduct(a, b), dotProduct(a, b));
   }
 
   static double angle(PointLong a, PointLong b) {
     return Math.atan2(crossProduct(a, b), dotProduct(a, b));
   }
 
+  static double angle(PointDouble a, PointDouble b, PointDouble origin) {
+    return angle(minus(a, origin), minus(b, origin));
+  }
+
   static double angle(PointLong a, PointLong b, PointLong origin) {
     return angle(minus(a, origin), minus(b, origin));
   }
 
-  /* static PointLong bisector(PointLong a, PointLong b) { */
-  /*   return plus(times(a, norm(b)), times(b, norm(a))); */
-  /* } */
+  static PointDouble bisector(PointDouble a, PointDouble b) {
+    return plus(times(a, norm(b)), times(b, norm(a)));
+  }
+
+  static PointDouble bisector(PointLong a, PointLong b) {
+    return plus(times(a.asPointDouble(), norm(b)), times(b.asPointDouble(), norm(a)));
+  }
+
+  static double clockwise(PointDouble a, PointDouble b) {
+    return crossProduct(b, a);
+  }
 
   static long clockwise(PointLong a, PointLong b) {
     return crossProduct(b, a);
+  }
+
+  static double clockwise(PointDouble a, PointDouble b, PointDouble origin) {
+    return clockwise(minus(a, origin), minus(b, origin));
   }
 
   static long clockwise(PointLong a, PointLong b, PointLong origin) {
     return clockwise(minus(a, origin), minus(b, origin));
   }
 
+  static double crossProduct(PointDouble a, PointDouble b) {
+    return a.x * b.y - a.y * b.x;
+  }
+
   static long crossProduct(PointLong a, PointLong b) {
     return a.x * b.y - a.y * b.x;
+  }
+
+  static double distance(PointDouble a, PointDouble b) {
+    return norm(minus(a, b));
   }
 
   static double distance(PointLong a, PointLong b) {
     return norm(minus(a, b));
   }
 
-  static PointLong divideBy(PointLong a, long factor) {
-    return new PointLong(a.x / factor, a.y / factor);
+  static PointDouble divideBy(PointDouble a, double factor) {
+    return new PointDouble(a.x / factor, a.y / factor);
+  }
+
+  static PointDouble divideBy(PointLong a, long factor) {
+    return new PointDouble(a.x / (double) factor, a.y / (double) factor);
+  }
+
+  static double dotProduct(PointDouble a, PointDouble b) {
+    return a.x * b.x + a.y * b.y;
   }
 
   static long dotProduct(PointLong a, PointLong b) {
     return a.x * b.x + a.y * b.y;
   }
 
+  static boolean greaterEqualTo(PointDouble a, PointDouble b) {
+    return !smallerThan(a, b);
+  }
+
   static boolean greaterEqualTo(PointLong a, PointLong b) {
     return !smallerThan(a, b);
+  }
+
+  static boolean greaterThan(PointDouble a, PointDouble b) {
+    return !smallerEqualTo(a, b);
   }
 
   static boolean greaterThan(PointLong a, PointLong b) {
     return !smallerEqualTo(a, b);
   }
 
+  static PointDouble minus(PointDouble a) {
+    return new PointDouble(-a.x, -a.y);
+  }
+
   static PointLong minus(PointLong a) {
     return new PointLong(-a.x, -a.y);
+  }
+
+  static PointDouble minus(PointDouble a, PointDouble b) {
+    return new PointDouble(a.x - b.x, a.y - b.y);
   }
 
   static PointLong minus(PointLong a, PointLong b) {
     return new PointLong(a.x - b.x, a.y - b.y);
   }
 
+  static double norm(PointDouble a) {
+    return Math.sqrt(dotProduct(a, a));
+  }
+
   static double norm(PointLong a) {
     return Math.sqrt(dotProduct(a, a));
+  }
+
+  static PointDouble perpendicular(PointDouble a) {
+    return new PointDouble(-a.y, a.x);
   }
 
   static PointLong perpendicular(PointLong a) {
     return new PointLong(-a.y, a.x);
   }
 
+  static PointDouble plus(PointDouble a, PointDouble b) {
+    return new PointDouble(a.x + b.x, a.y + b.y);
+  }
+
   static PointLong plus(PointLong a, PointLong b) {
     return new PointLong(a.x + b.x, a.y + b.y);
   }
 
-  static PointLong rotate(PointLong a, double angle) {
-    return new PointLong(
-        Math.round(Math.cos(angle) * a.x - Math.sin(angle) * a.y),
-        Math.round(Math.sin(angle) * a.x + Math.cos(angle) * a.y));
+  static PointDouble rotate(PointDouble a, double angle) {
+    return new PointDouble(
+        Math.cos(angle) * a.x - Math.sin(angle) * a.y,
+        Math.sin(angle) * a.x + Math.cos(angle) * a.y);
   }
 
-  static PointLong rotate(PointLong a, double angle, PointLong origin) {
+  static PointDouble rotate(PointLong a, double angle) {
+    return new PointDouble(
+        Math.cos(angle) * a.x - Math.sin(angle) * a.y,
+        Math.sin(angle) * a.x + Math.cos(angle) * a.y);
+  }
+
+  static PointDouble rotate(PointDouble a, double angle, PointDouble origin) {
     return plus(origin, rotate(minus(a, origin), angle));
+  }
+
+  static PointDouble rotate(PointLong a, double angle, PointLong origin) {
+    return plus(origin.asPointDouble(), rotate(minus(a, origin), angle));
+  }
+
+  static boolean smallerEqualTo(PointDouble a, PointDouble b) {
+    return a.x <= b.x || (a.x == b.x && a.y <= b.y);
   }
 
   static boolean smallerEqualTo(PointLong a, PointLong b) {
     return a.x <= b.x || (a.x == b.x && a.y <= b.y);
   }
 
+  static boolean smallerThan(PointDouble a, PointDouble b) {
+    return a.x < b.x || (a.x == b.x && a.y < b.y);
+  }
+
   static boolean smallerThan(PointLong a, PointLong b) {
     return a.x < b.x || (a.x == b.x && a.y < b.y);
   }
 
+  static PointDouble times(PointDouble a, double factor) {
+    return new PointDouble(a.x * factor, a.y * factor);
+  }
+
   static PointLong times(PointLong a, long factor) {
     return new PointLong(a.x * factor, a.y * factor);
+  }
+
+  static PointDouble times(PointDouble a, PointDouble b) {
+    return new PointDouble(a.x * b.x, a.y * b.y);
   }
 
   static PointLong times(PointLong a, PointLong b) {
@@ -217,6 +419,18 @@ public class TemplateComputationalGeometry {
 
   static boolean onLine(PointLong p, LineLong l) {
     return false;
+  }
+
+  static boolean onSegment(PointLong p, LineLong s) {
+    return false;
+  }
+
+  static double distanceToLine(PointLong p, LineLong l) {
+    return 0.0;
+  }
+
+  static double distanceToSegment(PointLong p, LineLong s) {
+    return 0.0;
   }
 
   abstract static class Polygon {}
