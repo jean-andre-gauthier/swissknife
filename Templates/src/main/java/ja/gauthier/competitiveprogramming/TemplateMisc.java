cac7681ee1dab386aecbf234fca48043e88a6a70
Jean André Gauthier <jean.andre.gauthier@gmail.com>
2017-05-23 21:30:10 +0200

    Helper methods for MinMaxQueues & stacks, fixed resizing bug

--- Templates/src/main/java/ja/gauthier/competitiveprogramming/TemplateMisc.java
+++ Templates/src/main/java/ja/gauthier/competitiveprogramming/TemplateMisc.java
@@ -3,6 +3,7 @@ package ja.gauthier.competitiveprogramming;
 import java.io.*;
 import java.lang.*;
 import java.util.*;
+import java.util.concurrent.*;
 import java.util.function.*;
 
 public class TemplateMisc {
@@ -87,101 +88,29 @@ public class TemplateMisc {
     int size() {
       return top + 1;
     }
-  }
 
-  static class MinMaxStackLong {
-    long[][] stack;
-    int top;
-
-    MinMaxStackLong(int capacity) {
-      stack = new long[capacity][3];
-      top = -1;
-    }
-
-    long getMax() {
-      return stack[top][2];
-    }
-
-    long getMin() {
-      return stack[top][1];
-    }
-
-    boolean isEmpty() {
-      return top == -1;
-    }
-
-    long pop() {
-      if (top == stack.length / 4) {
-        stack = Arrays.copyOf(stack, stack.length / 2);
+    public String toString() {
+      StringBuilder sb = new StringBuilder(stack.length * 6);
+      for (int j = 0; j < 3; ++j) {
+        toString(sb, j, false);
+        sb.append("\n");
       }
-      return stack[top--][0];
+      return sb.toString();
     }
 
-    void push(long i) {
-      if (top == stack.length - 1) {
-        int oldStackLength = stack.length;
-        int newStackLength = oldStackLength * 2;
-        stack = Arrays.copyOf(stack, newStackLength);
-        for (int j = oldStackLength; j < newStackLength; ++j) {
-          stack[j] = new long[3];
+    void toString(StringBuilder sb, int j, boolean reversed) {
+      if (reversed) {
+        for (int i = 0; i < size(); ++i) {
+          sb.append(stack[i][j] + " ");
+        }
+      } else {
+        for (int i = 0; i < size(); ++i) {
+          sb.append(stack[size() - i - 1][j] + " ");
         }
       }
-      top++;
-      stack[top][0] = i;
-      stack[top][1] = top == 0 ? i : Math.min(i, stack[top - 1][1]);
-      stack[top][2] = top == 0 ? i : Math.max(i, stack[top - 1][2]);
-    }
-
-    int size() {
-      return top + 1;
-    }
-  }
-
-  static class MinMaxStackDouble {
-    double[][] stack;
-    int top;
-
-    MinMaxStackDouble(int capacity) {
-      stack = new double[capacity][3];
-      top = -1;
-    }
-
-    double getMax() {
-      return stack[top][2];
-    }
-
-    double getMin() {
-      return stack[top][1];
-    }
-
-    boolean isEmpty() {
-      return top == -1;
-    }
-
-    double pop() {
-      if (top == stack.length / 4) {
-        stack = Arrays.copyOf(stack, stack.length / 2);
+      if (size() > 0) {
+        sb.deleteCharAt(sb.length() - 1);
       }
-      return stack[top--][0];
-    }
-
-    void push(double i) {
-      if (top == stack.length - 1) {
-        int oldStackLength = stack.length;
-        int newStackLength = oldStackLength * 2;
-        stack = Arrays.copyOf(stack, newStackLength);
-        for (int j = oldStackLength; j < newStackLength; ++j) {
-          stack[j] = new double[3];
-        }
-      }
-      top++;
-      stack[top][0] = i;
-      stack[top][1] = top == 0 ? i : Math.min(i, stack[top - 1][1]);
-      stack[top][2] = top == 0 ? i : Math.max(i, stack[top - 1][2]);
-    }
-
-    int size() {
-      return top + 1;
     }
   }
 
@@ -227,6 +156,279 @@ public class TemplateMisc {
 
     int size() {
       return stackIn.size() + stackOut.size();
+    }
+
+    public String toString() {
+      StringBuilder sb = new StringBuilder(size() * 6);
+      for (int j = 0; j < 3; ++j) {
+        stackIn.toString(sb, j, true);
+        if (stackIn.size() > 0) {
+          sb.append(" ");
+        }
+        stackOut.toString(sb, j, false);
+        sb.append("\n");
+      }
+      return sb.toString();
+    }
+  }
+
+  static class MinMaxStackLong {
+    long[][] stack;
+    int top;
+
+    MinMaxStackLong(int capacity) {
+      stack = new long[capacity][3];
+      top = -1;
+    }
+
+    long getMax() {
+      return stack[top][2];
+    }
+
+    long getMin() {
+      return stack[top][1];
+    }
+
+    boolean isEmpty() {
+      return top == -1;
+    }
+
+    long pop() {
+      if (stack.length >= 4 && top == stack.length / 4) {
+        stack = Arrays.copyOf(stack, Math.max(1, stack.length / 2));
+      }
+      return stack[top--][0];
+    }
+
+    void push(long i) {
+      if (top == stack.length - 1) {
+        int oldStackLength = stack.length;
+        int newStackLength = oldStackLength * 2;
+        stack = Arrays.copyOf(stack, newStackLength);
+        for (int j = oldStackLength; j < newStackLength; ++j) {
+          stack[j] = new long[3];
+        }
+      }
+      top++;
+      stack[top][0] = i;
+      stack[top][1] = top == 0 ? i : Math.min(i, stack[top - 1][1]);
+      stack[top][2] = top == 0 ? i : Math.max(i, stack[top - 1][2]);
+    }
+
+    int size() {
+      return top + 1;
+    }
+
+    public String toString() {
+      StringBuilder sb = new StringBuilder(stack.length * 6);
+      for (int j = 0; j < 3; ++j) {
+        toString(sb, j, false);
+        sb.append("\n");
+      }
+      return sb.toString();
+    }
+
+    void toString(StringBuilder sb, int j, boolean reversed) {
+      if (reversed) {
+        for (int i = 0; i < size(); ++i) {
+          sb.append(stack[i][j] + " ");
+        }
+      } else {
+        for (int i = 0; i < size(); ++i) {
+          sb.append(stack[size() - i - 1][j] + " ");
+        }
+      }
+      if (size() > 0) {
+        sb.deleteCharAt(sb.length() - 1);
+      }
+    }
+  }
+
+  static class MinMaxQueueLong {
+    MinMaxStackLong stackIn;
+    MinMaxStackLong stackOut;
+
+    MinMaxQueueLong(int stackCapacity) {
+      stackIn = new MinMaxStackLong(stackCapacity);
+      stackOut = new MinMaxStackLong(stackCapacity);
+    }
+
+    long dequeue() {
+      if (!stackOut.isEmpty()) {
+        return stackOut.pop();
+      } else {
+        while (!stackIn.isEmpty()) {
+          stackOut.push(stackIn.pop());
+        }
+        return stackOut.pop();
+      }
+    }
+
+    long getMax() {
+      return stackIn.isEmpty()
+          ? stackOut.getMax()
+          : stackOut.isEmpty() ? stackIn.getMax() : Math.max(stackIn.getMax(), stackOut.getMax());
+    }
+
+    long getMin() {
+      return stackIn.isEmpty()
+          ? stackOut.getMin()
+          : stackOut.isEmpty() ? stackIn.getMin() : Math.min(stackIn.getMin(), stackOut.getMin());
+    }
+
+    boolean isEmpty() {
+      return stackIn.isEmpty() && stackOut.isEmpty();
+    }
+
+    void enqueue(long i) {
+      stackIn.push(i);
+    }
+
+    int size() {
+      return stackIn.size() + stackOut.size();
+    }
+
+    public String toString() {
+      StringBuilder sb = new StringBuilder(size() * 6);
+      for (int j = 0; j < 3; ++j) {
+        stackIn.toString(sb, j, true);
+        if (stackIn.size() > 0) {
+          sb.append(" ");
+        }
+        stackOut.toString(sb, j, false);
+        sb.append("\n");
+      }
+      return sb.toString();
+    }
+  }
+
+  static class MinMaxStackDouble {
+    double[][] stack;
+    int top;
+
+    MinMaxStackDouble(int capacity) {
+      stack = new double[capacity][3];
+      top = -1;
+    }
+
+    double getMax() {
+      return stack[top][2];
+    }
+
+    double getMin() {
+      return stack[top][1];
+    }
+
+    boolean isEmpty() {
+      return top == -1;
+    }
+
+    double pop() {
+      if (stack.length >= 4 && top == stack.length / 4) {
+        stack = Arrays.copyOf(stack, Math.max(1, stack.length / 2));
+      }
+      return stack[top--][0];
+    }
+
+    void push(double i) {
+      if (top == stack.length - 1) {
+        int oldStackLength = stack.length;
+        int newStackLength = oldStackLength * 2;
+        stack = Arrays.copyOf(stack, newStackLength);
+        for (int j = oldStackLength; j < newStackLength; ++j) {
+          stack[j] = new double[3];
+        }
+      }
+      top++;
+      stack[top][0] = i;
+      stack[top][1] = top == 0 ? i : Math.min(i, stack[top - 1][1]);
+      stack[top][2] = top == 0 ? i : Math.max(i, stack[top - 1][2]);
+    }
+
+    int size() {
+      return top + 1;
+    }
+
+    public String toString() {
+      StringBuilder sb = new StringBuilder(stack.length * 6);
+      for (int j = 0; j < 3; ++j) {
+        toString(sb, j, false);
+        sb.append("\n");
+      }
+      return sb.toString();
+    }
+
+    void toString(StringBuilder sb, int j, boolean reversed) {
+      if (reversed) {
+        for (int i = 0; i < size(); ++i) {
+          sb.append(stack[i][j] + " ");
+        }
+      } else {
+        for (int i = 0; i < size(); ++i) {
+          sb.append(stack[size() - i - 1][j] + " ");
+        }
+      }
+      if (size() > 0) {
+        sb.deleteCharAt(sb.length() - 1);
+      }
+    }
+  }
+
+  static class MinMaxQueueDouble {
+    MinMaxStackDouble stackIn;
+    MinMaxStackDouble stackOut;
+
+    MinMaxQueueDouble(int stackCapacity) {
+      stackIn = new MinMaxStackDouble(stackCapacity);
+      stackOut = new MinMaxStackDouble(stackCapacity);
+    }
+
+    double dequeue() {
+      if (!stackOut.isEmpty()) {
+        return stackOut.pop();
+      } else {
+        while (!stackIn.isEmpty()) {
+          stackOut.push(stackIn.pop());
+        }
+        return stackOut.pop();
+      }
+    }
+
+    double getMax() {
+      return stackIn.isEmpty()
+          ? stackOut.getMax()
+          : stackOut.isEmpty() ? stackIn.getMax() : Math.max(stackIn.getMax(), stackOut.getMax());
+    }
+
+    double getMin() {
+      return stackIn.isEmpty()
+          ? stackOut.getMin()
+          : stackOut.isEmpty() ? stackIn.getMin() : Math.min(stackIn.getMin(), stackOut.getMin());
+    }
+
+    boolean isEmpty() {
+      return stackIn.isEmpty() && stackOut.isEmpty();
+    }
+
+    void enqueue(double i) {
+      stackIn.push(i);
+    }
+
+    int size() {
+      return stackIn.size() + stackOut.size();
+    }
+
+    public String toString() {
+      StringBuilder sb = new StringBuilder(size() * 6);
+      for (int j = 0; j < 3; ++j) {
+        stackIn.toString(sb, j, true);
+        if (stackIn.size() > 0) {
+          sb.append(" ");
+        }
+        stackOut.toString(sb, j, false);
+        sb.append("\n");
+      }
+      return sb.toString();
     }
   }
 
@@ -289,9 +491,5 @@ public class TemplateMisc {
     }
   }
 
-  public static void main(String[] args) {
-    MinMaxQueueInt s = new MinMaxQueueInt(1);
-    s.enqueue(0);
-    s.enqueue(1);
-  }
+  public static void main(String[] args) {}
 }
--- Templates/src/test/java/ja/gauthier/competitiveprogramming/MinMaxStackTest.java
+++ Templates/src/test/java/ja/gauthier/competitiveprogramming/MinMaxStackTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 
 public class MinMaxStackTest {
   @Test
-  public void getMaxTest() {
+  public void getMaxIntTest() {
     MinMaxStackInt s = new MinMaxStackInt(1);
     s.push(1);
     assertThat(s.getMax(), is(1));
@@ -24,7 +24,37 @@ public class MinMaxStackTest {
   }
 
   @Test
-  public void getMinTest() {
+  public void getMaxLongTest() {
+    MinMaxStackLong s = new MinMaxStackLong(1);
+    s.push(1L);
+    assertThat(s.getMax(), is(1L));
+    s.push(2L);
+    assertThat(s.getMax(), is(2L));
+    s.push(-1L);
+    assertThat(s.getMax(), is(2L));
+    s.pop();
+    assertThat(s.getMax(), is(2L));
+    s.pop();
+    assertThat(s.getMax(), is(1L));
+  }
+
+  @Test
+  public void getMaxDoubleTest() {
+    MinMaxStackDouble s = new MinMaxStackDouble(1);
+    s.push(1.0);
+    assertThat(s.getMax(), is(1.0));
+    s.push(2.0);
+    assertThat(s.getMax(), is(2.0));
+    s.push(-1.0);
+    assertThat(s.getMax(), is(2.0));
+    s.pop();
+    assertThat(s.getMax(), is(2.0));
+    s.pop();
+    assertThat(s.getMax(), is(1.0));
+  }
+
+  @Test
+  public void getMinIntTest() {
     MinMaxStackInt s = new MinMaxStackInt(1);
     s.push(1);
     assertThat(s.getMin(), is(1));
@@ -39,7 +69,37 @@ public class MinMaxStackTest {
   }
 
   @Test
-  public void emptyPopPushSizeTest() {
+  public void getMinLongTest() {
+    MinMaxStackLong s = new MinMaxStackLong(1);
+    s.push(1L);
+    assertThat(s.getMin(), is(1L));
+    s.push(2L);
+    assertThat(s.getMin(), is(1L));
+    s.push(-1L);
+    assertThat(s.getMin(), is(-1L));
+    s.pop();
+    assertThat(s.getMin(), is(1L));
+    s.pop();
+    assertThat(s.getMin(), is(1L));
+  }
+
+  @Test
+  public void getMinDoubleTest() {
+    MinMaxStackDouble s = new MinMaxStackDouble(1);
+    s.push(1.0);
+    assertThat(s.getMin(), is(1.0));
+    s.push(2.0);
+    assertThat(s.getMin(), is(1.0));
+    s.push(-1.0);
+    assertThat(s.getMin(), is(-1.0));
+    s.pop();
+    assertThat(s.getMin(), is(1.0));
+    s.pop();
+    assertThat(s.getMin(), is(1.0));
+  }
+
+  @Test
+  public void emptyPopPushSizeIntTest() {
     MinMaxStackInt s = new MinMaxStackInt(1);
     assertThat(s.isEmpty(), is(true));
     assertThat(s.size(), is(0));
@@ -70,7 +130,69 @@ public class MinMaxStackTest {
   }
 
   @Test
-  public void resizeTest() {
+  public void emptyPopPushSizeLongTest() {
+    MinMaxStackLong s = new MinMaxStackLong(1);
+    assertThat(s.isEmpty(), is(true));
+    assertThat(s.size(), is(0));
+    s.push(1L);
+    assertThat(s.isEmpty(), is(false));
+    assertThat(s.size(), is(1));
+    s.push(2L);
+    assertThat(s.isEmpty(), is(false));
+    assertThat(s.size(), is(2));
+    s.push(-1L);
+    assertThat(s.isEmpty(), is(false));
+    assertThat(s.size(), is(3));
+    assertThat(s.pop(), is(-1L));
+    assertThat(s.isEmpty(), is(false));
+    assertThat(s.size(), is(2));
+    s.push(-2L);
+    assertThat(s.isEmpty(), is(false));
+    assertThat(s.size(), is(3));
+    assertThat(s.pop(), is(-2L));
+    assertThat(s.isEmpty(), is(false));
+    assertThat(s.size(), is(2));
+    assertThat(s.pop(), is(2L));
+    assertThat(s.isEmpty(), is(false));
+    assertThat(s.size(), is(1));
+    assertThat(s.pop(), is(1L));
+    assertThat(s.isEmpty(), is(true));
+    assertThat(s.size(), is(0));
+  }
+
+  @Test
+  public void emptyPopPushSizeDoubleTest() {
+    MinMaxStackDouble s = new MinMaxStackDouble(1);
+    assertThat(s.isEmpty(), is(true));
+    assertThat(s.size(), is(0));
+    s.push(1.0);
+    assertThat(s.isEmpty(), is(false));
+    assertThat(s.size(), is(1));
+    s.push(2.0);
+    assertThat(s.isEmpty(), is(false));
+    assertThat(s.size(), is(2));
+    s.push(-1.0);
+    assertThat(s.isEmpty(), is(false));
+    assertThat(s.size(), is(3));
+    assertThat(s.pop(), is(-1.0));
+    assertThat(s.isEmpty(), is(false));
+    assertThat(s.size(), is(2));
+    s.push(-2.0);
+    assertThat(s.isEmpty(), is(false));
+    assertThat(s.size(), is(3));
+    assertThat(s.pop(), is(-2.0));
+    assertThat(s.isEmpty(), is(false));
+    assertThat(s.size(), is(2));
+    assertThat(s.pop(), is(2.0));
+    assertThat(s.isEmpty(), is(false));
+    assertThat(s.size(), is(1));
+    assertThat(s.pop(), is(1.0));
+    assertThat(s.isEmpty(), is(true));
+    assertThat(s.size(), is(0));
+  }
+
+  @Test
+  public void resizeIntTest() {
     MinMaxStackInt s = new MinMaxStackInt(1);
     int j = 0;
     while (++j <= 4) {
@@ -84,6 +206,50 @@ public class MinMaxStackTest {
         s.push(i);
       }
       for (int i = 127; i >= 0; --i) {
+        assertThat(s.pop(), is(i));
+      }
+      assertThat(s.isEmpty(), is(true));
+      assertThat(s.size(), is(0));
+    }
+  }
+
+  @Test
+  public void resizeLongTest() {
+    MinMaxStackLong s = new MinMaxStackLong(1);
+    long j = 0;
+    while (++j <= 4) {
+      for (long i = 0; i < 128L; ++i) {
+        s.push(i);
+      }
+      for (long i = 127; i >= 32L; --i) {
+        assertThat(s.pop(), is(i));
+      }
+      for (long i = 32; i < 128L; ++i) {
+        s.push(i);
+      }
+      for (long i = 127; i >= 0L; --i) {
+        assertThat(s.pop(), is(i));
+      }
+      assertThat(s.isEmpty(), is(true));
+      assertThat(s.size(), is(0));
+    }
+  }
+
+  @Test
+  public void resizeDoubleTest() {
+    MinMaxStackDouble s = new MinMaxStackDouble(1);
+    int j = 0;
+    while (++j <= 4) {
+      for (double i = 0.0; i < 128.0; ++i) {
+        s.push(i);
+      }
+      for (double i = 127.0; i >= 32.0; --i) {
+        assertThat(s.pop(), is(i));
+      }
+      for (double i = 32.0; i < 128.0; ++i) {
+        s.push(i);
+      }
+      for (double i = 127.0; i >= 0.0; --i) {
         assertThat(s.pop(), is(i));
       }
       assertThat(s.isEmpty(), is(true));
